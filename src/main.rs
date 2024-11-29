c6fd2d3016081b9750a80a09407b6e91086879d3
ayechan <ayechan.sant@gmail.com>
2024-11-29 12:36:28 -0800

    test went well no cap

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,35 @@ use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_third_person_camera::*;
 use camera::CameraPlugin;
-use helpers::{get_async_planets, get_planets, use_blender};
+// use helpers::{get_async_planets, get_planets, use_blender};
+use helpers::{get_planets, use_blender};
 use player::PlayerPlugin;
 use world::WorldPlugin;
-// fn main() {
-//     App::new()
-//         .add_plugins((
-//             DefaultPlugins,
-//             WorldPlugin,
-//             PlayerPlugin,
-//             CameraPlugin,
-//             WorldInspectorPlugin::new(),
-//             ThirdPersonCameraPlugin,
-//         ))
-//         .run();
-// }
 
-#[tokio::main]
-async fn main() {
-    let planets = get_async_planets();
-    for planet in planets.await.iter() {
-        match use_blender(planet).await {
-            Ok(response) => {
-                println!("Success: {:?}", response);
-            }
-            Err(e) => {
-                println!("Error sending planet to Flask: {:?}", e);
-            }
-        }
-    }
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            WorldPlugin,
+            PlayerPlugin,
+            CameraPlugin,
+            WorldInspectorPlugin::new(),
+            ThirdPersonCameraPlugin,
+        ))
+        .run();
 }
+
+// #[tokio::main]
+// async fn main() {
+//     let planets = get_async_planets();
+//     for planet in planets.await.iter() {
+//         match use_blender(planet).await {
+//             Ok(response) => {
+//                 println!("Success: {:?}", response);
+//             }
+//             Err(e) => {
+//                 println!("Error sending planet to Flask: {:?}", e);
+//             }
+//         }
+//     }
+// }
